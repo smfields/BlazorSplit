2e4d1e32eed276a75e4dd09826cc01de2671fcff
Sam Fields <smfields896@gmail.com>
2022-03-12 01:24:22 -0500

    Various UI improvements

---
+++ samples/BlazorSplit.Samples/BlazorSplit.Shared/PreviewConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BlazorSplit.OptionTypes;
+
+namespace BlazorSplit.Shared;
+
+public class PreviewConfiguration
+{
+    public Direction Direction { get; set; }
+    
+    public int Columns { get; set; }
+    
+    public int MinimumSize { get; set; }
+    
+    public int MaximumSize { get; set; }
+    
+    public int GutterSize { get; set; }
+    
+    public GutterAlign GutterAlignment { get; set; }
+    
+    public int SnapOffset { get; set; }
+    
+    public int DragInterval { get; set; }
+}
